ab23cec3b2a5f625d374394f7324880d739d6ed3
Md.Nafiul Haque <54739121+nafiulhaqueinan@users.noreply.github.com>
2023-03-14 10:34:57 +0600

    Add files via upload

---
+++ Static Stack.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Stack{
+private:
+	int st[100],i=-1,m=100;
+public:
+	bool isEmpty(){
+		if(i==-1)
+			return true;
+		else
+			return false;
+	}
+	bool isFull(){
+		if(i==m){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	bool push(int x){
+		if(!isFull()){
+			i++;
+			st[i]=x;
+		}
+	}
+	bool pop(){
+		if(isEmpty()){
+			cout<<"Empty!\n";
+		}else{
+			i--;
+		}
+	}
+	int topElement(){
+		return st[i];
+	}
+	void show(){
+		int k=i+1;
+		while(k--){
+			cout<<st[k]<<endl;
+		}
+	}
+};
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+	// int n;
+	// cout<<"Input the Size of Stack : ";cin>>n;
+	// stack[n];
+	// int i=0;
+
+	Stack s;
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.pop();
+	s.pop();
+	s.push(100);
+	cout<<s.topElement()<<endl;
+	s.show();
+
+	
+	return 0;
+
+}
